ed6b98202e799b3cdf7fce98d0ef7bb6c9d853e6
jpatel531 <jamie@notespublication.com>
2014-08-31 16:03:37 +0100

    Added public activity gem. Wrote news_feed_spec

---
+++ spec/features/news_feed_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'news feed' do 
+
+	let(:user_one) { User.create email: 'hello@hi.com', password: '12345678', password_confirmation: '12345678'}
+	let(:user_two) {User.create email: "sup@lo.com", password: '12345678', password_confirmation: '12345678'}
+	before { Post.create title: "Pretty Picture"}
+
+	context 'likes broadcast', js: true do 
+
+		it 'will alert me when somebody has liked a post' do 
+			login_as user_one
+			visit '/'
+			# save_and_open_page
+			click_link '♥ 0'
+			logout(:user_one)
+			login_as user_two
+			visit '/'
+			expect(page).to have_content 'hello@hi.com liked Pretty Picture'
+
+		end
+
+	end
+
+
+end
